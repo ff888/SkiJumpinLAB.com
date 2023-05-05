1e22d01791c4c3707f3122231df36cc7cfa36c29
Jakub Krajewski <jakub.j.krajewski@gmail.com>
2023-05-05 11:22:18 +0100

    Add statistis filter engine

--- live_info_scraper.py
+++
@@ -1,4 +0,0 @@
-"""
-Script for scrap information from fis-web about coming events.
-"""
-
--- ski/views.py
+++ ski/views.py
@@ -1,4 +1,5 @@
 import os
+from .utils.ststistics_helpers import get_categories
 import pandas as pd
 
 from django.shortcuts import render
@@ -23,33 +24,37 @@ def fantasy_league(request):
 def statistics(request):
     # directory where CSV files are located
     csv_folder = 'media/ski_db'
-    # list of CSV files
+    # list of CSV files target only tournaments after 2001
     csv_files = [f for f in os.listdir(csv_folder) if f.endswith('.csv') and f.split('_')[0] >= '2002']
+
+    # Get category values for filters
+    categories = get_categories(csv_files)
 
     # Check if the request method is GET
     if request.method == 'GET':
-        # Get the value of the 'csv_file' parameter from the request's GET dictionary
-        csv_file = request.GET.get('csv_file')
+        # Get the filter values from the request's GET dictionary
+        season_filter = request.GET.get('season_filter')
+        city_filter = request.GET.get('city_filter')
+        tournament_filter = request.GET.get('tournament_filter')
+        hill_filter = request.GET.get('hill_filter')
+        gender_filter = request.GET.get('gender_filter')
+        team_filter = request.GET.get('team_filter')
 
-        # if parameter was passed in the request
-        if csv_file in csv_files:
-            # path to selected CSV file
-            file_path = os.path.join(csv_folder, csv_file)
-            # create a pandas dataframe
-            df = pd.read_csv(file_path)
-            # replace all spaces in columns names with underscores
-            df.columns = df.columns.str.replace(' ', '_')
-            # Convert the dataframe to a list of dictionaries representing each row
-            rows = df.to_dict('records')
-            # Set the title
-            title = f"Statistics - {csv_file}"
-            # Set the table title
-            table_title = csv_file
-            return render(request, 'ski/statistics.html', {'rows': rows, 'title': title, 'table_title': table_title})
+        # Filter the csv_files based on the filter values
+        filtered_csv_files = [file for file in csv_files if
+                              (city_filter is None or city_filter in file) and
+                              (hill_filter is None or hill_filter in file) and
+                              (season_filter is None or season_filter in file) and
+                              (tournament_filter is None or tournament_filter in file) and
+                              (gender_filter is None or gender_filter in file) and
+                              (team_filter is None or team_filter in file)]
 
-    return render(request, 'ski/statistics.html', {'csv_files': csv_files, 'title': 'Statistics'})
+        filtered_csv_files = sorted(filtered_csv_files, key=lambda file: file.split('_')[0], reverse=True)
+
+        # render the files list in your template
+        return render(request, 'ski/statistics.html',
+                      {'filtered_csv_files': filtered_csv_files, 'categories': categories, 'csv_files': csv_files})
 
 
 def blog(request):
-
     return render(request, 'ski/blog.html', {'title': 'Blog'})
