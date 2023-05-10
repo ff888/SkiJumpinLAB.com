74110041b8928a412b3608ffd8e6aecdb5b5ce7a
Jakub Krajewski <jakub.j.krajewski@gmail.com>
2023-05-10 12:28:32 +0100

    Add function to scrape information about upcoming events from a specified source and display it

--- ski/views.py
+++ ski/views.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 from .utils.ststistics_helpers import get_categories, files_by_year, files_by_season, get_four_hills_files, \
     get_raw_air_files
-from .utils.live_helpers import get_date_list
+from .utils.live_helpers import get_date_list, get_event_info
 from django.shortcuts import render
 
 
@@ -16,7 +16,7 @@ def about(request):
 
 
 def live(request):
-    date_list = get_date_list()
+    date_list = get_event_info(get_date_list())
     return render(request, 'ski/live.html', {'title': 'LIVE', 'date_list': date_list})
 
 
